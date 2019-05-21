404a61659bad59917806ca8228ed930885181d1a
Sander Brugge <sander.brugge@moovly.com>
2019-05-21 12:09:13 +0200

    finished the product services and added a cache

--- src/api/Product/product.services.ts
+++ src/api/Product/product.services.ts
@@ -1,6 +1,9 @@
 import data from "./products.json";
 import { Product } from "./product.js";
 
+// Add a product cache, so we can store and retrieve fetched products, without needing to wait for the call to finish.
+const productCache = new Map<string, Promise<Product>>();
+
 /**
  * Fetch all of the products.
  */
@@ -9,10 +12,19 @@ export function fetchProducts(): Promise<Product[]> {
 }
 
 /**
- * retrieve a single product by ID.
+ * Retrieve a single product by ID.
+ * Checks if the product has been previously retrieved and stored in cache.
  * @param id the product's ID to retrieve
  */
 export function getProductById(id: string): Promise<Product> {
+  if (productCache.has(id)) {
+    return productCache.get(id) as Promise<Product>;
+  }
+
   const product = data.find((product: Product) => product.id === id);
-  return new Promise(resolve => setTimeout(() => resolve(product), 1000));
+  const promise: Promise<Product> = new Promise(resolve =>
+    setTimeout(() => resolve(product), 1000)
+  );
+  productCache.set(id, promise);
+  return promise;
 }
--- src/components/Item/ItemContainer.tsx
+++ src/components/Item/ItemContainer.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import { Item } from "../../api/Order/order";
 import { View, Text } from "react-native";
 import ItemStyles from "./Item.styles";
+import ItemInformation from "./ItemInformation";
+import { getProductById } from "../../api/Product/product.services";
+import { Product } from "../../api/Product/product";
 
 interface IProps {
   item: Item;
@@ -12,23 +15,28 @@ interface IProps {
  * @param param0 the item to display
  */
 const ItemContainer: React.FC<IProps> = ({ item }) => {
+  const [product, setProduct] = React.useState<Product | undefined>(undefined);
+  const productName = product ? product.description : item["product-id"];
+  React.useEffect(() => {
+    const fetchProductData = async () => {
+      try {
+        const data = await getProductById(item["product-id"]);
+        setProduct(data);
+      } catch (error) {
+        console.log(error);
+        // can be used for additional error handling
+      }
+    };
+    fetchProductData();
+  }, []);
   return (
     <View style={ItemStyles.container}>
-      <Text style={ItemStyles.productName}>{item["product-id"]}</Text>
+      <Text style={ItemStyles.productName}>{productName}</Text>
       <Text style={ItemStyles.productId}>{item["product-id"]}</Text>
       <View style={ItemStyles.overviewContainer}>
-        <View style={ItemStyles.orderInformation}>
-          <Text style={ItemStyles.informationHeader}>QUANTITY</Text>
-          <Text style={ItemStyles.informationDetail}>{item.quantity}</Text>
-        </View>
-        <View style={ItemStyles.orderInformation}>
-          <Text style={ItemStyles.informationHeader}>UNIT PRICE</Text>
-          <Text style={ItemStyles.informationDetail}>{item["unit-price"]}</Text>
-        </View>
-        <View style={ItemStyles.orderInformation}>
-          <Text style={ItemStyles.informationHeader}>TOTAL</Text>
-          <Text style={ItemStyles.informationDetail}>${item.total}</Text>
-        </View>
+        <ItemInformation title="QUANTITY" info={item.quantity} />
+        <ItemInformation title="UNIT PRICE" info={item["unit-price"]} />
+        <ItemInformation title="TOTAL" info={item.total} />
       </View>
     </View>
   );
