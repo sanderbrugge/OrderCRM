c5bfecdcc4ce5c0f3e928efea08e6b355ad8741f
Sander Brugge <sander.brugge@moovly.com>
2019-05-22 09:48:29 +0200

    extract styles from addproduct

---
+++ src/scenes/AddProduct/AddProduct.styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const AddProductStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "600"
+  },
+  bodyContainer: {
+    margin: 10
+  }
+});
+
+export default AddProductStyles;
